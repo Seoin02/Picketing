e4d648446fbbbaef65c11e7988a95fe32721ecd3
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-09 21:00:17 +0900

    fix: 크롬 확장 popup 경로 재설정

--- packages/Picketing/vite.config.ts
+++ packages/Picketing/vite.config.ts
@@ -1,7 +1,14 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 });
