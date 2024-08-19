71497099a7bd660db2ae03e4ba07d09730ea0eef
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-19 11:10:23 +0900

    feat: 기본 Input 컴포넌트 생성

---
+++ packages/design-system/src/stories/Input/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type InputProps = React.ComponentPropsWithRef<'input'> & {
+  invalid: boolean;
+};
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ invalid = false, className, ...props }, ref) => {
+  return <input {...props} ref={ref} className={className} />;
+});
+
+Input.displayName = 'Input';
+
+export default Input;
