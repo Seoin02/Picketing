d7b57d3c9111e3d7f7a64c2ac408863741727603
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-19 12:15:41 +0900

    feat: Input story 작성

---
+++ packages/design-system/src/stories/Input/Input.stories.ts
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Input, { InputProps } from '.';
+
+const meta: Meta<typeof Input> = {
+  title: 'Input',
+  component: Input,
+  argTypes: {
+    invalid: { control: 'boolean' },
+    className: {
+      control: 'text',
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<InputProps>;
+
+export const Default: Story = {
+  args: {
+    invalid: false,
+    className: 'w-4 h-1 border border-gold-50',
+  },
+};
+
+export const Invalid: Story = {
+  args: {
+    invalid: true,
+    className: 'w-4 h-1 border border-red',
+  },
+};
