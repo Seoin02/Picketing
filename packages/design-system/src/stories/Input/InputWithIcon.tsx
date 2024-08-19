c0d8f0589d7fcc3be55815a4c7fcc4f92232d4b9
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-19 11:43:51 +0900

    feat: InputWithIcon 컴포넌트 생성

--- packages/design-system/src/stories/Input/InputWithIcon.tsx
+++ packages/design-system/src/stories/Input/InputWithIcon.tsx
@@ -1,3 +1,19 @@
-import { InputProps } from './index';
+import React from 'react';
+import Input, { InputProps } from './index';
 
-type InputWithIcon = InputProps;
+type InputWithIconProps = InputProps & {
+  icon: string;
+};
+
+const InputWithIcon = React.forwardRef<HTMLInputElement, InputWithIconProps>(({ icon }) => {
+  return (
+    <div>
+      <Input invalid={false} />
+      <img src={icon} />
+    </div>
+  );
+});
+
+InputWithIcon.displayName = 'InputWithIcon';
+
+export default InputWithIcon;
