b4c5ed7c594584925beb743c17b980db904fe16d
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-22 18:49:34 +0900

    test: 서버 시간 출력 확인

--- packages/picketing/src/pages/Popup.tsx
+++ packages/picketing/src/pages/Popup.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Dropdown from '../../../design-system/src/stories/Dropdown';
+import fetchServerTime from '../shared/apis/serverTime/api';
 
 const Popup = () => {
+  const [serverTime, setServerTime] = useState<string>('Loading...');
+  console.log(serverTime.slice(17, 26));
+  useEffect(() => {
+    const getServerTime = setInterval(async () => {
+      const time = await fetchServerTime(window.location.href);
+      setServerTime(time);
+    }, 1000);
+
+    return () => clearInterval(getServerTime);
+  }, []);
+
   return (
     <>
-      <h1>
+      <h1 className="text-sm">
         해당 페이지의 서버시간은
         <br />
-        입니다.
+        {serverTime.slice(17, 26)}입니다.
       </h1>
       <Dropdown title="다른 사이트 서버 시간" width="small" />
     </>
