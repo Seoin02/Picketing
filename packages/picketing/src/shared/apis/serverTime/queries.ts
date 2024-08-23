6536021579962684912ebd80ebfdf3389f1932ca
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-23 14:39:55 +0900

    feat: serverTime query key 정리

---
+++ packages/picketing/src/shared/apis/serverTime/queries.ts
@@ -0,0 +1,17 @@
+import { queryClient } from '../../utils/queryClient';
+import fetchServerTime from './api';
+
+const key = {
+  timeData: (url: string) => ['timeData', url],
+};
+
+export const timeQueries = {
+  getQueryKey: (url: string) => key.timeData(url),
+  removeQuery: (url: string) => queryClient.removeQueries({ queryKey: timeQueries.getQueryKey(url) }),
+  queryOptions: (url: string) => {
+    return {
+      queryKey: timeQueries.getQueryKey(url),
+      queryFn: () => fetchServerTime(url),
+    };
+  },
+};
